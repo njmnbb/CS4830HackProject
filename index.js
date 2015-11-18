1385ad7cfe38e76fa4efe2e05d25faadb223da01
Daniel J. Hart <dhart94@gmail.com>
2015-11-18 22:57:04 +0000

    Fixed resource loading on port 3000

--- index.js
+++ index.js
@@ -8,17 +8,8 @@ var debug = 0;
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/images');
-});
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/main.css');
+	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
