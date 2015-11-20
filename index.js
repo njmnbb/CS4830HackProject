21efe42d7b4871d5a39b8e4bbf0fd0835ef3dd4c
Nick Martini <njmnbb@mail.missouri.edu>
2015-11-20 00:17:52 -0600

    Fixed listening error in index.js

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var app = express();
+var server = app.listen(3000, function(){
+	console.log('listening on *:3000');
+});
+var io = require('socket.io').listen(server);
 
 // http://blog.modulus.io/nodejs-and-express-static-content
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
-	//console.log('a user connected');
- //  socket.on('simon message', function(msg){
- //    io.emit('simon message', msg);
-	// console.log(msg);
- //  });
-});;
-
-app.listen(process.env.PORT || 3000, function(){
-	console.log('listening on *:3000');
+	console.log('a user connected');
+  socket.on('simon message', function(msg){
+    io.emit('simon message', msg);
+	console.log(msg);
+  });
 });
+
